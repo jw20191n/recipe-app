90ea174b8b75a48334c70b7cba88e36b1cc510b3
jw20191n <clairejw1015@gmail.com>
2020-11-30 16:09:17 -0500

    basic shopping list form working

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+  // ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem(){
+    let name = this.nameInputRef.nativeElement.value;
+    let amount = this.amountInputRef.nativeElement.value;
+    let newIngredient = new Ingredient(name, amount);
+
+    //emit the event and notify the parent component that a new ingredient has been added
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
